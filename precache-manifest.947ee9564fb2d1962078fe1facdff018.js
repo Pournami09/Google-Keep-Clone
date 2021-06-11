05bcaf233de44a025afd680f2776b3f77ff8a1c6
pournami09 <pournami.pottekat@gmail.com>
2021-06-11 14:37:59 +0530

    Updates

---
+++ precache-manifest.947ee9564fb2d1962078fe1facdff018.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd93d357079421baf7651d7d8b02b407",
+    "url": "/Google-Keep-Clone/index.html"
+  },
+  {
+    "revision": "714f5f6bed40fd36a472",
+    "url": "/Google-Keep-Clone/static/js/2.96b8ecf1.chunk.js"
+  },
+  {
+    "revision": "a03b8d17ea267f49e806",
+    "url": "/Google-Keep-Clone/static/js/main.251f86ab.chunk.js"
+  },
+  {
+    "revision": "148dbd174bcfb032234e",
+    "url": "/Google-Keep-Clone/static/js/runtime-main.17a7dd08.js"
+  }
+]);
